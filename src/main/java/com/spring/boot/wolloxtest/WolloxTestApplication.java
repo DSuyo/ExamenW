f0188a07964a7dbfd89807eb765f3a747edef84b
Roxana Camara <roxana.camara07@gmail.com>
2021-02-18 17:04:22 -0300

    first commit

---
+++ src/main/java/com/spring/boot/wolloxtest/WolloxTestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.wolloxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolloxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WolloxTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/boot/wolloxtest/WolloxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.wolloxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolloxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
